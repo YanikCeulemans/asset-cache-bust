623b57ac76992eaecd27465bd656a827a2b11a97
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2017-02-18 16:59:20 +0100

    do nothing on no cache busting+ refactors * glob will no longer match directories, only files * Updated help tekst for CLI to better reflect what it does * Updated package.json description * Added and rearranged tests

--- cli.js
+++ cli.js
@@ -11,35 +11,40 @@ const cacheBustHtml = require('./index.js');
 //    getFileNamesFromPattern : String -> Task e (List String)
 const getFileNamesFromPattern = pattern => {
     return new Task((reject, resolve) =>
-        glob(pattern, {nonull: false}, (err, matches) => {
+        glob(pattern, {nonull: false, nodir: true}, (err, matches) => {
             if (err) return reject(err);
-            if (matches.length === 0) return reject({ message: `No files could be found that match given glob: '${pattern}'` });
+            if (matches.length === 0) return reject({ message: [`No files could be found that match given glob: '${pattern}'`, `Directory searched for matches: ${process.cwd()}`].join('\n') });
             return resolve(List(matches));
         })
     );
 };
 
-//    writeToPath : String -> String -> Task e ()
-const writeToPath = pathName => contents => {
-    return List(path.normalize(path.dirname(pathName)).split(path.sep))
-        .map((d, index, dirs) => path.join(...dirs.toArray().slice(0, index + 1)))
-        .map(dirPath => stat(dirPath)
-            // TODO: Using stat to check for existence is not recommended, refactor. See: https://nodejs.org/api/fs.html#fs_fs_stat_path_callback
-            .chain(stats => stats.isDirectory() ? Task.of() : mkdir(dirPath))
-            .orElse((err) => mkdir(dirPath))
-        )
-        .reduce((acc, curr) => acc.chain(a => curr), Task.of())
-        .chain(() => writeFile(pathName)(contents))
-        .map(() => console.log('Wrote file: ' + chalk.bold(pathName)));
+const checkChangesMade = originalContents => changedContents => {
+    return new Task((reject, resolve) => {
+        if (originalContents === changedContents) return reject('Original contents and changed contents are equal');
+        resolve(changedContents);
+    });
 };
 
-const cacheBustFromFilePath = filePath => {
-    return readFile(filePath)
-        .map(fileContents => {
-            console.log('Read file: ' + chalk.bold(filePath));
-            return fileContents;
-        })
-        .chain(cacheBustHtml);
+//    writeToPath : String -> String -> Task e ()
+const writeToPath = pathName => originalContents => contents => {
+    return checkChangesMade(originalContents)(contents)
+        .chain(() => 
+            List(path.normalize(path.dirname(pathName)).split(path.sep))
+                .map((d, index, dirs) => path.join(...dirs.toArray().slice(0, index + 1)))
+                .map(dirPath => stat(dirPath)
+                    // TODO: Using stat to check for existence is not recommended, refactor. See: https://nodejs.org/api/fs.html#fs_fs_stat_path_callback
+                    .chain(stats => stats.isDirectory() ? Task.of() : mkdir(dirPath))
+                    .orElse((err) => mkdir(dirPath))
+                )
+                .reduce((acc, curr) => acc.chain(a => curr), Task.of())
+                .chain(() => writeFile(pathName)(contents))
+                .map(() => console.log('Wrote file: ' + chalk.bold(pathName)))
+        )
+        .orElse(() => {
+            console.log('No cache busting replacements have been made. Exiting.');
+            return Task.of();
+        });
 };
 
 //    cacheBustFromGlob : String -> String -> ()
@@ -49,8 +54,15 @@ const cacheBustFromGlob = pattern => outPath => {
         .chain(filePaths =>
             filePaths
                 .map(filePath =>
-                    cacheBustFromFilePath(filePath)
-                        .chain(writeToPath(path.join((outPath || '.').trim(), filePath)))
+                    readFile(filePath)
+                        .map(fileContents => {
+                            console.log('Read file: ' + chalk.bold(filePath));
+                            return fileContents;
+                        })
+                        .chain(originalHtmlContents =>
+                            cacheBustHtml(originalHtmlContents)
+                                .chain(writeToPath(path.join((outPath || '.').trim(), filePath))(originalHtmlContents))
+                        )
                 )
                 .reduce((acc, curr) => acc.chain(a => curr), Task.of())
         )
@@ -71,7 +83,7 @@ const cli = meow(`
         <input>     Required    A glob that should be used to locate files as templates for fingerprinting
     
     Options
-        -o, --output  Send fingerprinted HTML output to given file path instead of overwriting the input files
+        -o, --output  Send fingerprinted HTML output to given output directory path instead of overwriting the input files
  
     Examples
       $ ${package.name} index.html -o out/
--- test/index.js
+++ test/index.js
@@ -1,35 +1,68 @@
 const cacheBustHtml = require('../index.js');
 
 describe('asset-cache-bust', () => {
-    it('Should not do anything given an empty string', done => {
-        cacheBustHtml('')
-            .fork(done, () => done());
-    });
     it('Should not do anything given null', done => {
         cacheBustHtml()
-            .fork(done, () => done());
+            .fork(done, (result) => {
+                if (result != null) return done(Error());
+                done()
+            });
     });
-    it('Should return its input html when no matching tags were found', done => {
-        const html = '<link href="nonexistant.css" rel="stylesheet" />';
+    it('Should not do anything given an empty string', done => {
+        cacheBustHtml('')
+            .fork(done, (result) => {
+                if (result !== '') return done(Error());
+                done()
+            });
+    });
+    it('Should not do anything given invalid HTML', done => {
+        const html = 'link href="test/existant.css" rel="stylesheet" data-finger-print';
         cacheBustHtml(html)
-            .fork(done, bustedHtml => {
-                if (bustedHtml !== html) done(Error());
-                done();
+            .fork(done, (result) => {
+                if (result !== html) return done(Error());
+                done()
             });
     });
     it('Should skip assets that could not be found', done => {
         const html = '<link href="nonexistant.css" rel="stylesheet" data-finger-print />';
         cacheBustHtml(html)
             .fork(done, bustedHtml => {
-                if (bustedHtml !== html) done(Error());
+                if (bustedHtml !== html) return done(Error());
                 done();
+            });
+    });
+    it('Should skip assets that that are not contained in a matching tag', done => {
+        const html = `
+            <link href="test/existant.css" rel="stylesheet" data-finger-print />
+            <link href="nonexistant.css" rel="stylesheet" />
+        `;
+        cacheBustHtml(html)
+            .fork(done, bustedHtml => {
+                if (bustedHtml.indexOf('href="nonexistant.css"') === -1) return done(Error());
+                done();
+            })
+    });
+    it('Should skip links that do not have an href attribute', done => {
+        const html = '<link rel="stylesheet" data-finger-print />';
+        cacheBustHtml(html)
+            .fork(done, (result) => {
+                if (result !== html) return done(Error());
+                done()
+            });
+    });
+    it('Should skip links that have an empty href attribute', done => {
+        const html = '<link href="" rel="stylesheet" data-finger-print />';
+        cacheBustHtml(html)
+            .fork(done, (result) => {
+                if (result !== html) return done(Error());
+                done()
             });
     });
     it('Should process assets and add finger prints to their urls', done => {
         const html = '<link href="test/existant.css" rel="stylesheet" data-finger-print />';
         cacheBustHtml(html)
             .fork(done, bustedHtml => {
-                if (bustedHtml === html) done(Error());
+                if (!/"test\/existant\.css\?v=([a-z0-9])+"/.test(bustedHtml)) return done(Error());
                 done();
             });
     });
